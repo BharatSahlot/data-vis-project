4ce0b8d114ea8c283174168578f014cbc7f3ae70
BharatSahlot <bharatsahlot18@gmail.com>
2023-04-10 16:13:48 +0530

    remove downloaded files

---
+++ Data/src/products.js
@@ -0,0 +1,21 @@
+const PRODUCTS = [
+    { name: 'Animal', code: '01-05_Animal' },
+    { name: 'Vegetable', code: '06-15_Vegetable' },
+    { name: 'Food Products', code: '16-24_FoodProd' },
+    { name: 'Minerals', code: '25-26_Minerals' },
+    { name: 'Fuels', code: '27-27_Fuels' },
+    { name: 'Chemicals', code: '28-38_Chemicals' },
+    { name: 'Plastic or Rubber', code: '39-40_PlastiRub' },
+    { name: 'Hides and Skins', code: '41-43_HidesSkin' },
+    { name: 'Wood', code: '44-49_Wood' },
+    { name: 'Textiles and Clothing', code: '50-63_TextCloth' },
+    { name: 'Footwear', code: '64-67_Footwear' },
+    { name: 'Stone and Glass', code: '68-71_StoneGlas' },
+    { name: 'Metals', code: '72-83_Metals' },
+    { name: 'Mach and Elec', code: '84-85_MachElec' },
+    { name: 'Transportation', code: '86-89_Transport' },
+    { name: 'Miscellaneous', code: '90-99_Miscellan' },
+    { name: 'All Products', code: 'Total' },
+];
+
+export default PRODUCTS;
