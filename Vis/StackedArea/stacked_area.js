47ba190b759015d0c5fe2f2dbd0ba2e87f0209fb
BharatSahlot <bharatsahlot18@gmail.com>
2023-04-26 20:26:40 +0530

    animate timeline graph

--- Vis/StackedArea/stacked_area.js
+++ Vis/StackedArea/stacked_area.js
@@ -1,5 +1,298 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
+function AddH1N1Info(svg, xScale, yScale, yPos)
+{
+    const year = 2009;
+    const delay = (year - 1988) * 150;
+
+    svg.append("line")
+        .attr("x1", xScale(2009))
+        .attr("x2", xScale(2009))
+        .attr("y1", yScale(0))
+        .attr("y2", yScale(0))
+        .attr("stroke", "lightgray")
+        .attr("stroke-dasharray", 4)
+        .transition()
+        .duration(1500)
+        .delay(delay)
+        .attr("y2", yPos);
+
+    svg.append("circle")
+        .attr("cx", xScale(2009))
+        .attr("cy", yPos)
+        .attr("r", 0)
+        .attr("fill", "white")
+        .transition()
+        .duration(200)
+        .delay(delay + 1400)
+        .attr("r", 7);
+
+    svg.append("text")
+        .attr("x", xScale(2009) + 10)
+        .attr("y", yPos + 20)
+        .attr("fill", "white")
+        .attr("opacity", 0)
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .delay(delay + 1600)
+        .attr("opacity", 1)
+        .text("H1N1")
+
+    svg.append("text")
+        .attr("x", xScale(2009) + 10)
+        .attr("y", yPos + 40)
+        .attr("fill", "gray")
+        .attr("opacity", 0)
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .delay(delay + 1600)
+        .attr("opacity", 1)
+        .text("The 2009 H1N1 flu pandemic is")
+
+    svg.append("text")
+        .attr("x", xScale(2009) + 10)
+        .attr("y", yPos + 60)
+        .attr("fill", "gray")
+        .attr("opacity", 0)
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .delay(delay + 1600)
+        .attr("opacity", 1)
+        .text("estimated to have actually")
+
+    svg.append("text")
+        .attr("x", xScale(2009) + 10)
+        .attr("y", yPos + 80)
+        .attr("fill", "gray")
+        .attr("opacity", 0)
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .delay(delay + 1600)
+        .attr("opacity", 1)
+        .text("caused about 284,000 deaths.")
+}
+
+
+function Add911Info(svg, xScale, yScale, yPos)
+{
+    const year = 2001.75;
+    const delay = (year - 1988) * 150;
+
+    svg.append("line")
+        .attr("x1", xScale(year))
+        .attr("x2", xScale(year))
+        .attr("y1", yScale(0))
+        .attr("y2", yScale(0))
+        .attr("stroke", "lightgray")
+        .attr("stroke-dasharray", 4)
+        .transition()
+        .duration(1500)
+        .delay(delay)
+        .attr("y2", yPos);
+
+    svg.append("circle")
+        .attr("cx", xScale(year))
+        .attr("cy", yPos)
+        .attr("r", 0)
+        .attr("fill", "white")
+        .transition()
+        .duration(200)
+        .delay(delay + 1400)
+        .attr("r", 7);
+
+    svg.append("text")
+        .attr("x", xScale(year) + 10)
+        .attr("y", yPos + 20)
+        .attr("fill", "white")
+        .attr("opacity", 0)
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .delay(delay + 1600)
+        .attr("opacity", 1)
+        .text("9/11 Attacks")
+
+    svg.append("text")
+        .attr("x", xScale(year) + 10)
+        .attr("y", yPos + 40)
+        .attr("fill", "gray")
+        .attr("opacity", 0)
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .delay(delay + 1600)
+        .attr("opacity", 1)
+        .text("The attacks had a significant")
+
+    svg.append("text")
+        .attr("x", xScale(year) + 10)
+        .attr("y", yPos + 60)
+        .attr("fill", "gray")
+        .attr("opacity", 0)
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .delay(delay + 1600)
+        .attr("opacity", 1)
+        .text("economic impact on United States")
+
+    svg.append("text")
+        .attr("x", xScale(year) + 10)
+        .attr("y", yPos + 80)
+        .attr("fill", "gray")
+        .attr("opacity", 0)
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .delay(delay + 1600)
+        .attr("opacity", 1)
+        .text("and world markets.")
+}
+
+function Add2016Info(svg, xScale, yScale, yPos)
+{
+    const year = 2016;
+    const delay = (year - 1988) * 150;
+
+    svg.append("line")
+        .attr("x1", xScale(year))
+        .attr("x2", xScale(year))
+        .attr("y1", yScale(0))
+        .attr("y2", yScale(0))
+        .attr("stroke", "lightgray")
+        .attr("stroke-dasharray", 4)
+        .transition()
+        .duration(1500)
+        .delay(delay)
+        .attr("y2", yPos);
+
+    svg.append("circle")
+        .attr("cx", xScale(year))
+        .attr("cy", yPos)
+        .attr("r", 0)
+        .attr("fill", "white")
+        .transition()
+        .duration(200)
+        .delay(delay + 1400)
+        .attr("r", 7);
+
+    svg.append("text")
+        .attr("x", xScale(year) - 50)
+        .attr("y", yPos + 20)
+        .attr("fill", "white")
+        .attr("opacity", 0)
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .delay(delay + 1600)
+        .attr("opacity", 1)
+        .text("2016")
+
+    svg.append("text")
+        .attr("x", xScale(year) - 163)
+        .attr("y", yPos + 40)
+        .attr("fill", "gray")
+        .attr("opacity", 0)
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .delay(delay + 1600)
+        .attr("opacity", 1)
+        .text("Zika Virus Pandemic")
+
+    svg.append("text")
+        .attr("x", xScale(year) - 240)
+        .attr("y", yPos + 60)
+        .attr("fill", "gray")
+        .attr("opacity", 0)
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .delay(delay + 1600)
+        .attr("opacity", 1)
+        .text("Trump elected as US President")
+}
+
+function AddCoronaInfo(svg, xScale, yScale, yPos)
+{
+    const year = 2019;
+    const delay = (year - 1988) * 150;
+
+    svg.append("line")
+        .attr("x1", xScale(year))
+        .attr("x2", xScale(year))
+        .attr("y1", yScale(0))
+        .attr("y2", yScale(0))
+        .attr("stroke", "lightgray")
+        .attr("stroke-dasharray", 4)
+        .transition()
+        .duration(1500)
+        .delay(delay)
+        .attr("y2", yPos);
+
+    svg.append("circle")
+        .attr("cx", xScale(year))
+        .attr("cy", yPos)
+        .attr("r", 0)
+        .attr("fill", "white")
+        .transition()
+        .duration(200)
+        .delay(delay + 1400)
+        .attr("r", 7);
+
+    svg.append("text")
+        .attr("x", xScale(year) - 80)
+        .attr("y", yPos + 20)
+        .attr("fill", "white")
+        .attr("opacity", 0)
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .delay(delay + 1600)
+        .attr("opacity", 1)
+        .text("COVID19")
+
+    svg.append("text")
+        .attr("x", xScale(year) - 153)
+        .attr("y", yPos + 40)
+        .attr("fill", "gray")
+        .attr("opacity", 0)
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .delay(delay + 1600)
+        .attr("opacity", 1)
+        .text("As of 25 April 2023,")
+
+    svg.append("text")
+        .attr("x", xScale(year) - 205)
+        .attr("y", yPos + 60)
+        .attr("fill", "gray")
+        .attr("opacity", 0)
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .delay(delay + 1600)
+        .attr("opacity", 1)
+        .text("the pandemic had caused")
+
+    svg.append("text")
+        .attr("x", xScale(year) - 220)
+        .attr("y", yPos + 80)
+        .attr("fill", "gray")
+        .attr("opacity", 0)
+        .transition()
+        .ease(d3.easeLinear)
+        .duration(200)
+        .delay(delay + 1600)
+        .attr("opacity", 1)
+        .text("6,908,541 confirmed deaths")
+}
+
 function ShowGraph(data, config)
 {
     console.log(data);
@@ -27,16 +320,16 @@ function ShowGraph(data, config)
 
     const color = d3.interpolateMagma;
 
-    const labelOffsetY = 75;
+    const labelOffsetY = 100;
 
     svg.append("text")
-        .attr("x", config.width - 157)
+        .attr("x", 15)
         .attr("y", labelOffsetY - 40)
         .style("fill", "lightgray")
         .text("Top 10(2020)");
 
     svg.append("text")
-        .attr("x", config.width - 157)
+        .attr("x", 15)
         .attr("y", labelOffsetY - 20)
         .style("fill", "lightgray")
         .text("Countries");
@@ -90,23 +383,28 @@ function ShowGraph(data, config)
         if(cnt.si >= 10) continue;
 
         svg.append("circle")
-            .attr("cx", config.width - 150)
+            .attr("cx", 15 + 3.5)
             .attr("cy", labelOffsetY + 25 * cnt.si)
             .attr("r", 7)
             .attr("fill", col)
 
         svg.append("text")
-            .attr("x", config.width - 140)
+            .attr("x", 15 + 3.5 + 10)
             .attr("y", labelOffsetY + 5 + 25 * cnt.si)
             .style("fill", "gray")
             .text(cnt.country);
     }
 
+    AddH1N1Info(svg, xScale, yScale, config.height * 0.3);
+    Add911Info(svg, xScale, yScale, config.height * 0.5);
+    Add2016Info(svg, xScale, yScale, config.height * 0.2);
+    AddCoronaInfo(svg, xScale, yScale, config.height * 0.05);
+
     const mouseText = svg.append("text")
-        .attr("x", config.width - 230)
+        .attr("x", 20)
         .attr("y", config.height - 15)
         .style("fill", "white")
-        .text("Year: 2020, Value: 12321321");
+        .text("Year: 2020, Value: 0");
 
     svg.on("mousemove", e => {
         const xPerc = e.clientX / config.width;
